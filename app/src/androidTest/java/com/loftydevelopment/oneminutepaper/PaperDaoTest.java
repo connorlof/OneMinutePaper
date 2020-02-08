56d532b0a7c14ec29370970d775614becca9aefa
Connor Loftus <connorlof@gmail.com>
2020-02-08 17:44:11 -0500

    added PaperDao instrumentation tests

--- app/src/androidTest/java/com/loftydevelopment/oneminutepaper/PaperDaoTest.java
+++ app/src/androidTest/java/com/loftydevelopment/oneminutepaper/PaperDaoTest.java
@@ -1,4 +1,108 @@
 package com.loftydevelopment.oneminutepaper;
 
-public class PaperDaoTest {
+import android.database.sqlite.SQLiteConstraintException;
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+
+import com.loftydevelopment.oneminutepaper.model.Paper;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.List;
+
+public class PaperDaoTest extends PaperDatabaseTest{
+
+    public static final String TEST_SUBJECT = "Science";
+    public static final String TEST_MAIN_IDEAS = "Gravity is only a theory.";
+    public static final String TEST_QUESTION = "Why can I not fly?";
+
+    public static final Paper TEST_PAPER_1 =
+            new Paper("Math", "You cannot divide by 0.", "Why can you multiply by 0?");
+
+
+    @Rule
+    public InstantTaskExecutorRule rule = new InstantTaskExecutorRule();
+
+    /*
+        Insert, read, delete
+     */
+    @Test
+    public void insertReadDelete() throws Exception{
+
+        Paper paper = new Paper(TEST_PAPER_1);
+
+        //insert
+        getPaperDao().insertPaper(paper);
+
+        //read
+        List<Paper> insertedPapers = getPaperDao().loadAllPapers();
+
+        Assert.assertNotNull(insertedPapers);
+
+        Assert.assertEquals(paper.getSubject(), insertedPapers.get(0).getSubject());
+        Assert.assertEquals(paper.getMainIdeas(), insertedPapers.get(0).getMainIdeas());
+        Assert.assertEquals(paper.getQuestions(), insertedPapers.get(0).getQuestions());
+
+        paper.setId(insertedPapers.get(0).getId());
+        Assert.assertEquals(paper, insertedPapers.get(0));
+
+        //delete
+        getPaperDao().deletePaper(paper);
+
+        //confirm the database is empty
+        insertedPapers = getPaperDao().loadAllPapers();
+        Assert.assertEquals(0, insertedPapers.size());
+
+    }
+
+    /*
+        Insert, read, update, read, delete
+     */
+    @Test
+    public void insertReadUpdateReadDelete() throws Exception{
+
+        Paper paper = new Paper(TEST_PAPER_1);
+
+        //insert
+        getPaperDao().insertPaper(paper);
+
+        //read
+        List<Paper> insertedPapers = getPaperDao().loadAllPapers();
+
+        Assert.assertNotNull(insertedPapers);
+
+        Assert.assertEquals(paper.getSubject(), insertedPapers.get(0).getSubject());
+        Assert.assertEquals(paper.getMainIdeas(), insertedPapers.get(0).getMainIdeas());
+        Assert.assertEquals(paper.getQuestions(), insertedPapers.get(0).getQuestions());
+
+        paper.setId(insertedPapers.get(0).getId());
+        Assert.assertEquals(paper, insertedPapers.get(0));
+
+        //update
+        paper.setSubject(TEST_SUBJECT);
+        paper.setMainIdeas(TEST_MAIN_IDEAS);
+        paper.setQuestions(TEST_QUESTION);
+        getPaperDao().updatePaper(paper);
+
+        //read
+        insertedPapers = getPaperDao().loadAllPapers();
+
+        Assert.assertEquals(TEST_SUBJECT, insertedPapers.get(0).getSubject());
+        Assert.assertEquals(TEST_MAIN_IDEAS, insertedPapers.get(0).getMainIdeas());
+        Assert.assertEquals(TEST_QUESTION, insertedPapers.get(0).getQuestions());
+
+        paper.setId(insertedPapers.get(0).getId());
+        Assert.assertEquals(paper, insertedPapers.get(0));
+
+        //delete
+        getPaperDao().deletePaper(paper);
+
+        //confirm the database is empty
+        insertedPapers = getPaperDao().loadAllPapers();
+        Assert.assertEquals(0, insertedPapers.size());
+
+    }
+
 }
--- app/src/androidTest/java/com/loftydevelopment/oneminutepaper/PaperDatabaseTest.java
+++ app/src/androidTest/java/com/loftydevelopment/oneminutepaper/PaperDatabaseTest.java
@@ -1,4 +1,35 @@
 package com.loftydevelopment.oneminutepaper;
 
-public class PaperDatabaseTest {
+import androidx.room.Room;
+import androidx.test.core.app.ApplicationProvider;
+
+
+import com.loftydevelopment.oneminutepaper.persistence.PaperDao;
+import com.loftydevelopment.oneminutepaper.persistence.PaperDatabase;
+
+import org.junit.After;
+import org.junit.Before;
+
+public abstract class PaperDatabaseTest {
+
+    private PaperDatabase paperDatabase;
+
+    public PaperDao getPaperDao(){
+        return paperDatabase.paperDao();
+    }
+
+    @Before
+    public void init() {
+        paperDatabase = Room.inMemoryDatabaseBuilder(
+                ApplicationProvider.getApplicationContext(),
+                PaperDatabase.class
+        ).build();
+
+    }
+
+    @After
+    public void finish() {
+        paperDatabase.close();
+    }
+
 }
